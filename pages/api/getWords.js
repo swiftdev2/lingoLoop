dbc998da8d51267f52fac9e1619b1ecce8c1cb20
adam3002 <iamadam3002@gmail.com>
2024-10-04 23:57:46 +0400

    fixed groups filter

--- pages/api/getWords.js
+++ pages/api/getWords.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
 
       res.status(200).json(rows);
     } else {
-      if (groups.includes("all")) {
+      if (groups.toLowerCase().includes("all")) {
         const [rows] = await connection.execute(
           "SELECT * FROM words WHERE shown=TRUE",
         );
@@ -31,23 +31,29 @@ export default async function handler(req, res) {
           incorrectWords: incorrectRows,
         });
       } else {
-        const sql = `
-          SELECT * 
-          FROM words 
-          AND (
-            FIND_IN_SET(wordGroups, ?) > 0
-          )
-        `;
+        const groupsArray = groups.split(",").map((group) => group.trim());
 
-        // Execute the query
-        const [rows] = await connection.execute(sql, [groups]);
+        console.log("groupsArray", groupsArray);
+        // Construct the SQL query
+        const sql = "SELECT * FROM words";
+        const [rows] = await connection.execute(sql);
+        const filteredResults = rows.filter((row) => {
+          // Split the wordGroups into an array and trim whitespace
+          const wordGroupsArray = row.wordGroups
+            .split(",")
+            .map((group) => group.trim());
+
+          return wordGroupsArray.some((wordGroup) =>
+            groupsArray.includes(wordGroup.toLowerCase()),
+          );
+        });
 
         const [incorrectRows] = await connection.execute(
           "SELECT * FROM incorrectWords",
         );
 
         res.status(200).json({
-          words: rows,
+          words: filteredResults,
           incorrectWords: incorrectRows,
         });
       }
